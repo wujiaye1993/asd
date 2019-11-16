4e8a514f3cad1e675774188fd91422e6874c90d5
wujiaye <594663945@qq.com>
2019-11-16 19:30:33 +0800

    asdasdasdad

---
+++ Ron.SignalRLesson1/Controllers/SignalRController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ron.SignalRLesson1.Controllers
+{
+    public class SignalRController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
